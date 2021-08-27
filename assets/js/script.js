b496a3159b9303d280a8376f21fc97469fe54f7d
Salim66 <salimhasanriad8@gmail.com>
2021-08-27 21:07:47 +0600

    All functionality done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
 
+// grab everything we need
+let buttons = document.querySelectorAll('.move_button');
+
+
+// deifne our function w'll need
+function moveButton(){
+
+    //Get a random number for the top, left offset
+    const offset_top   = Math.random() * (window.innerHeight - this.clientHeight);
+    const offset_left  = Math.random() * (window.innerWidth - this.clientWidth);
+
+
+    //apply those number to the buttons
+    this.style.top  = offset_top + 'px';
+    this.style.left = offset_left + 'px';
+}
+
+// add event listener
+buttons.forEach(button => button.addEventListener('mouseenter', moveButton));
